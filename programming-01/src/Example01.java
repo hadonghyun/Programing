18e12128ef6de40414130b18f751b6b17f0f2ec7
hadonghyun <hadonghyeon7172@gmail.com>
2025-03-10 09:44:53 +0900

    2025/03/10 프로그래밍 2강 수업
    Java로 문자열 출력

---
+++ programming-01/src/Example01.java
@@ -0,0 +1,7 @@
+public class Example01 {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world!");
+        System.out.println("hello world!"); //단락바꾸면 자동으로 줄바꿈 (\n안써도 됨)
+    }
+}
---
+++ programming-01/src/Example02.java
@@ -0,0 +1,6 @@
+public class Example02 {
+    public static void main(String[] args){
+        System.out.print("AA");
+        System.out.print("BB");//이건 줄안바꿔도 됨
+    }
+}
